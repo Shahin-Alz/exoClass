1d9650c3a94ebf0c79de4be0d617bc5251161ced
Shahin-Alz <shaalemzadeh@gmail.com>
2023-01-04 12:55:58 +0100

    ajout

---
+++ public/js/main.js
@@ -0,0 +1,23 @@
+//EXO1
+
+
+// class personnage {
+//     constructor(age, nom, ville){
+//         this.age = age 
+//         this.nom = nom
+//         this.ville = ville
+//     }
+    
+//     sePresenter(){
+//         console.log(`Bonjour je m'appel ${this.nom}`);
+//     }
+    
+    
+// }
+
+// let Shahin = new personnage(22, "Shahin","Bxl")
+// Shahin.sePresenter()
+
+// let Gia = new personnage(24, "Gianni", "Ath")
+// Gia.sePresenter()
+
